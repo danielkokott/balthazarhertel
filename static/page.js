96415e01dc7cfc5a36a5f265567dfdaaa60ea3c9
Daniel Kokott <daniel@kokott.dk>
2013-06-28 23:37:49 +0200

    site verification

--- static/page.js
+++ static/page.js
@@ -1,7 +1,7 @@
 $(".moviethumb").click(function(e) {
-	if ($(this).attr("play") !== undefined) {
-		if ($("#movieplayer").attr("src") != $(this).attr("play")) {
-			$("#movieplayer").attr("src",$(this).attr("play"));
+	if ($(this).attr("data-play") !== undefined) {
+		if ($("#movieplayer").attr("src") != $(this).attr("data-play")) {
+			$("#movieplayer").attr("src",$(this).attr("data-play"));
 		}
 		else{
 			e.preventDefault();
@@ -16,6 +16,8 @@ $(".moviethumb").click(function(e) {
 $(".moviethumb").hover(
 	function() {
 		$(this).next(".movielabel").children("p").show();
+		$(this).prev(".movielabel").children("img").show();
+		console.log($(this).prev(".movielabel").children("img").attr("src"));
 	},
 	function() {
 		$(this).next(".movielabel").children("p").hide();
