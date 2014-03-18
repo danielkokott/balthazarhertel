72d82b4c4c00b3e1181be83ae58937437a803974
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2014-03-18 16:41:12 +0100

    Revert "work in progress"
    This reverts commit 11e408c214e5a8b0ce11a46deb4a2e3eb697e908.

--- js/balthazarhertel.js
+++ js/balthazarhertel.js
@@ -1,69 +1,18 @@
-var app = angular.module('balthazarhertel', ['ngRoute', 'ngSanitize'])
-.config(function($routeProvider, $locationProvider) {
-    //$locationProvider.html5Mode(true);
-    $routeProvider.when('/', {
-        templateUrl: 'index.html',
-        controller: MovieCtrl
-    });
-
-    $routeProvider.when('/:movie', {
-        templateUrl: 'index.html',
-        controller: MovieCtrl
-    });
-});
-app.directive("moviethumb", function() {
-    var linkFunction = function(scope, element, attributes) {
-        console.log("scope");
-
-        scope.h1 = "BAM";
-        scope.title = "PAM";
-    }
-
-    return {
-        restrict: 'E',
-        templateUrl: '../templates/moviethumb.html',
-        link: linkFunction
-    }
+$(".movie").click(function(e) {
+	if ($(this).attr("data-play") !== undefined) {
+		if ($("#movieplayer iframe").attr("src") != $(this).attr("data-play")) {
+			$("#movieplayer iframe").attr("src",$(this).attr("data-play"));
+		}
+		else {
+			e.preventDefault();
+		}
+	}
+	else {
+		e.preventDefault();
+	}
+	$("#movieplayer").slideDown(500);
 });
 
-function MovieCtrl($scope, $http, $location, $routeParams) {
-    $scope.movies = movies;
-    console.log($scope.movies);
-    $location.path("/tilalleminevenner");
-}
-
-var movies = [
-    {
-        "url": "http://player.vimeo.com/video/74717033",
-        "thumb": "img/TAMV.jpg",
-        "h1": "SUPER16",
-        "title": "TIL ALLE MINE VENNER",
-        "slug": "tilalleminevenner"
-    },
-    {
-        "url": "http://player.vimeo.com/video/73302141",
-        "thumb": "img/Trold.jpg",
-        "h1": "KORTFILM",
-        "title": "TROLD",
-        "slug": "trold"
-    }
-];
-
-// $(".movie").click(function(e) {
-// 	if ($(this).attr("data-play") !== undefined) {
-// 		if ($("#movieplayer iframe").attr("src") != $(this).attr("data-play")) {
-// 			$("#movieplayer iframe").attr("src",$(this).attr("data-play"));
-// 		}angular.module('contentdb_poc', ['ngRoute', 'ngSanitize'])
-// 		else {
-// 			e.preventDefault();
-// 		}
-// 	}
-// 	else {
-// 		e.preventDefault();
-// 	}
-// 	$("#movieplayer").slideDown(500);
-// });
-
-// $("#movieplay_close").click(function(e) {
-// 	$("#movieplayer").slideUp(500);
-// });
+$("#movieplay_close").click(function(e) {
+	$("#movieplayer").slideUp(500);
+});
